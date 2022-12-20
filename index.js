43a6adac38a634ec478afb892ab7a45dd25bfad5
sans-wd <alientofq1@gmail.com>
2022-12-20 09:22:06 +0400

    dd

---
+++ index.js
@@ -0,0 +1,24 @@
+var something = [
+  ["Oneplus", "Samsung", "Pixel"],
+  [
+    "Cyberpunk2077",
+    "Red Dead Redemtion 2",
+    "JoJos Bizarre Adventure All-Star Battle R",
+    "Hotline Miami",
+  ],
+  ["hoody", "boots", "t-shirt", "sneakers"],
+];
+
+console.log(
+  "Hi. My name is" +
+    " " +
+    something[0][1][0] +
+    something[1][0][3] +
+    something[1][2][16] +
+    something[2][2][4] +
+    something[2][3][4] +
+    " " +
+    "and I'm fan of" +
+    " " +
+    something[1][0]
+);
